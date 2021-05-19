5b6d484434b5391ba791162b8e73137c054b41a8
Nicklas Millard <n.millard@me.com>
2021-05-19 09:28:21 +0200

    rename solution

--- src/ManagingSecrets/ConsoleSecretsApp/Program.cs
+++ src/ManagingSecrets/ConsoleSecretsApp/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace ConsoleSecretsApp {
-    public static class Program {
+﻿using System;
+using System.IO;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleSecretsApp {
+    public class Program {
         public static void Main(string[] args) {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ENVIRONMENT")}.json")
+                .AddUserSecrets<Program>()
+                .AddAzureKeyVault(
+                    new Uri($"https://{Environment.GetEnvironmentVariable("AZ_KEYVAULT")}.vault.azure.net/"),
+                    new DefaultAzureCredential())
+                .Build();
             
+            Console.WriteLine(config["Something:MyValue"]);
         }
     }
 }
