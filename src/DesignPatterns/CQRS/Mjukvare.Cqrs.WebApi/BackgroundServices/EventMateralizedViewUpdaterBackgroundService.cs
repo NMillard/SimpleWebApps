b08b91c33fb8534326742a21812c88de486808b2
Nicklas Millard <n.millard@me.com>
2024-11-15 14:58:05 +0100

    Refactor

--- src/DesignPatterns/CQRS/Mjukvare.Cqrs.WebApi/BackgroundServices/EventMateralizedViewUpdaterBackgroundService.cs
+++ src/DesignPatterns/CQRS/Mjukvare.Cqrs.WebApi/BackgroundServices/EventMateralizedViewUpdaterBackgroundService.cs
@@ -33,48 +33,65 @@ public sealed class ChannelMaterializedViewUpdaterBackgroundService(
             using IServiceScope scope = serviceProvider.CreateScope();
             await using var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
+            logger.LogInformation("Updating materialized view for checkin {CheckinId}", id);
+
             UserCheckinDisplay? checkin = await context.UserCheckinDisplays
                 .AsNoTracking()
                 .SingleOrDefaultAsync(u => u.UserId == id, stoppingToken);
-            
-            UserCheckinDisplay? result = await context.Users
-                .AsNoTracking()
-                .Include(u => u.Checkins)
-                .Select(u => new UserCheckinDisplay
-                {
-                    UserId = u.Id,
-                    Username = u.Username,
-                    TotalCheckins = u.Checkins.Count,
-                    LatestCheckinDate = u.Checkins.OrderByDescending(c => c.Created).Select(c => c.Created).FirstOrDefault(),
-                    Checkins = u.Checkins.Select(c => new CheckinDisplay
-                    {
-                        CheckinId = c.Id,
-                        Text = c.Text,
-                        Created = c.Created
-                    }).ToList()
-                })
-                .SingleOrDefaultAsync(u => u.UserId == id, stoppingToken);
-            
+
             if (checkin is null)
             {
+                UserCheckinDisplay? result = await context.Users
+                    .AsNoTracking()
+                    .Include(u => u.Checkins)
+                    .Select(u => new UserCheckinDisplay
+                    {
+                        UserId = u.Id,
+                        Username = u.Username,
+                        TotalCheckins = u.Checkins.Count,
+                        LatestCheckinDate = u.Checkins.OrderByDescending(c => c.Created).Select(c => c.Created)
+                            .FirstOrDefault(),
+                        Checkins = u.Checkins.Select(c => new CheckinDisplay
+                        {
+                            CheckinId = c.Id,
+                            Text = c.Text,
+                            Created = c.Created
+                        }).ToList()
+                    })
+                    .SingleOrDefaultAsync(u => u.UserId == id, stoppingToken);
                 context.UserCheckinDisplays.Add(result);
-            }
-            else
-            {
-                context.UserCheckinDisplays.Update(result);
-            }
 
-            try
-            {
-                await context.SaveChangesAsync(stoppingToken);
-            }
-            catch (DbUpdateException e)
-            {
-                logger.LogError(e, "Error updating materialized view for checkin {CheckinId}", id);
+                try
+                {
+                    await context.SaveChangesAsync(stoppingToken);
+                }
+                catch (DbUpdateException e)
+                {
+                    logger.LogError(e, "Error updating materialized view for checkin {CheckinId}", id);
+                }
+
                 continue;
             }
-            
-            logger.LogInformation("Updating materialized view for checkin {CheckinId}", id);
+
+            List<CheckinDisplay> updatedCheckins = context.Users
+                .Include(u => u.Checkins)
+                .Where(u => u.Id == id)
+                .SelectMany(u => u.Checkins)
+                .Select(c => new CheckinDisplay
+                {
+                    CheckinId = c.Id,
+                    Created = c.Created,
+                    Text = c.Text
+                })
+                .ToList();
+
+            await context.UserCheckinDisplays
+                .Where(u => u.UserId == id)
+                .ExecuteUpdateAsync(
+                    setter => setter
+                        .SetProperty(display => display.TotalCheckins, updatedCheckins.Count)
+                        .SetProperty(display => display.Checkins, updatedCheckins),
+                    cancellationToken: stoppingToken);
         }
     }
 }
